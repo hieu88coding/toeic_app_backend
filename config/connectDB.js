f6b8b539b8588d658c3eb99e7b6918b075f0fdfa
Hiếu Cao <91723664+hieu88coding@users.noreply.github.com>
2024-06-21 21:56:42 +0700

    fix password

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const dotenv = require('dotenv')
 dotenv.config()
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWPRD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false //de k hien query moi khi reload trang
