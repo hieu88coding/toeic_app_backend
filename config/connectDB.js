098e870947b82bb3e4b5741063729efc9a563ba5
Hiếu Cao <91723664+hieu88coding@users.noreply.github.com>
2024-06-23 16:20:32 +0700

    test login

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
 const dotenv = require('dotenv')
 dotenv.config()
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+// deploy mode
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false //de k hien query moi khi reload trang
+// });
+const sequelize = new Sequelize('toeic_app', 'root', null, {
+    host: '127.0.0.1',
     dialect: 'mysql',
     logging: false //de k hien query moi khi reload trang
 });
